9cd4315f392d5385ace60bd77ca06de235d26660
Pavlo <pavlo.escola@gmail.com>
2025-05-30 18:16:47 +0100

    Syncronized tip of index finger to mouse

--- hand_recognition.py
+++ hand_recognition.py
@@ -1,6 +1,11 @@
 import cv2
 import mediapipe as mp
+import tkinter as tk
+import mouse
 
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
 
@@ -19,17 +24,20 @@ while cap.isOpened():
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
     lm_dict = {}
+    mouse_coordiantes = ()
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             # Draw landmarks on the frame
             for id, point in enumerate(hand_landmarks.landmark):
-                print(id, point)
                 h,w,c= frame.shape
                 cx,cy=int(point.x * w) , int(point.y * h)
-                lm_dict[id] = [cx,cy]
+                lm_dict[id] = [point.x,point.y]
                 cv2.putText(frame, str(id), (cx+5,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1, cv2.LINE_AA)
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            mouse_coordiantes = (int(lm_dict[8][0] * screen_width), int(lm_dict[8][1] * screen_height))
+            mouse.move(mouse_coordiantes[0], mouse_coordiantes[1])
+            print(mouse_coordiantes)
     cv2.imshow('Hand Recognition', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
