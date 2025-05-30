249084d333473ce9968b090ad798171dc0316869
Pavlo <pavlo.escola@gmail.com>
2025-05-30 19:26:21 +0100

    Added mouse movement

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 
 This project serves the purpose of utlising AI to recognise hand gestures and integrate it to allow to control your computer using hand gesture alone.
 
+## Done
+
+- Open camera
+- Detect hand landmarks
+- Move mouse in accordance to index finger-tip
+## To Do:
+
+- Calculate gestures using euclidean distance
+- Calculate closed hand distance to warn user if he is too close or too far
+- GUI Interface (Minimise after)
+## Random commands for myself
+
 source .venv/bin/activate
 
 pip install -r requirements.txt
--- hand_recognition.py
+++ hand_recognition.py
@@ -2,6 +2,8 @@ import cv2
 import mediapipe as mp
 import tkinter as tk
 import mouse
+import autopy
+import math
 
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
@@ -14,6 +16,16 @@ mp_drawing = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 assert cap.isOpened()
+
+
+def gestures(lm_dict):
+    palm = lm_dict[0]
+    tips_to_palm = math.dist(lm_dict[4], palm) + math.dist(lm_dict[8], palm) + math.dist(lm_dict[12], palm) + math.dist(lm_dict[16], palm) + math.dist(lm_dict[20], palm)
+    if tips_to_palm > 1.3:
+        print("open hand")
+    else:
+        print("closed hand")
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -35,11 +47,14 @@ while cap.isOpened():
                 lm_dict[id] = [point.x,point.y]
                 cv2.putText(frame, str(id), (cx+5,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1, cv2.LINE_AA)
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            mouse_coordiantes = (int(lm_dict[8][0] * screen_width), int(lm_dict[8][1] * screen_height))
-            mouse.move(mouse_coordiantes[0], mouse_coordiantes[1])
-            print(mouse_coordiantes)
+            
+        #Move mouse
+        mouse_coordiantes = (int(lm_dict[8][0] * screen_width), int(lm_dict[8][1] * screen_height))
+        #autopy.mouse.smooth_move(mouse_coordiantes[0], mouse_coordiantes[1])
+        autopy.mouse.move(mouse_coordiantes[0], mouse_coordiantes[1])
+        #mouse.move(mouse_coordiantes[0], mouse_coordiantes[1])
+        gestures(lm_dict)
     cv2.imshow('Hand Recognition', frame)
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
