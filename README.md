323041f6b02652b353fa89aa47a3f8229916d0e4
Pavlo <pavlo.escola@gmail.com>
2025-05-30 01:19:54 +0100

    added hand numbers

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 # Hand PC Control (AI)
 
 This project serves the purpose of utlising AI to recognise hand gestures and integrate it to allow to control your computer using hand gesture alone.
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
---
+++ hand_recognition.py
@@ -0,0 +1,40 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1)
+
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+assert cap.isOpened()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("not ret")
+        continue
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(frame_rgb)
+    lm_dict = {}
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Draw landmarks on the frame
+            for id, point in enumerate(hand_landmarks.landmark):
+                print(id, point)
+                h,w,c= frame.shape
+                cx,cy=int(point.x * w) , int(point.y * h)
+                lm_dict[id] = [cx,cy]
+                cv2.putText(frame, str(id), (cx+5,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1, cv2.LINE_AA)
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    cv2.imshow('Hand Recognition', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
